430166aaa38e8802481ecac45fd6714b2218bebf
Wonderfull23 <mywonderkotlin23@gmail.com>
2023-02-08 02:00:44 +0300

    stage_4 - added function for save changes in file after user's right answers, added functionality for users answers

--- src/main/kotlin/additional/File.kt
+++ src/main/kotlin/additional/File.kt
@@ -2,8 +2,10 @@ package additional
 
 import java.io.File
 
+val wordsFile = File("words.txt")
+
 fun main() {
-    val wordsFile = File("words.txt")
+
     wordsFile.createNewFile()
 
     val listOfWords = wordsFile.readLines()
@@ -31,34 +33,61 @@ fun createDirectory(listOfWords: List<String>): MutableList<Word> {
 fun menu(dictionary: MutableList<Word>) {
     while (true) {
         val answerVariants = 4
+        val learnTimes = 3
         println("Меню:\n1 – Учить слова\n2 – Статистика\n0 – Выход")
         when (readln()) {
             "1" -> {
                 val randomWords =
-                    dictionary.filter { it.correctAnswersCount < 3 }.shuffled().take(answerVariants).toMutableList()
+                    dictionary.filter { it.correctAnswersCount < learnTimes }.shuffled().take(answerVariants)
+                        .toMutableList()
                 if (randomWords.isEmpty()) {
                     println("Вы выучили все слова")
                     break
                 } else if (randomWords.size < answerVariants)
-                    randomWords += dictionary.filter { it.correctAnswersCount > 3 }.shuffled()
+                    randomWords += dictionary.filter { it.correctAnswersCount >= learnTimes }.shuffled()
                         .take(answerVariants - randomWords.size)
 
-                val word = randomWords.filter { it.correctAnswersCount < 3 }.random().original
-                println(word)
-                randomWords.forEachIndexed { index, randomWord -> print("$index - ${randomWord.translate}" +
-                        if (index != randomWords.lastIndex) ", " else "\n") }
+                val originalWord = randomWords.filter { it.correctAnswersCount < learnTimes }.random().original
+                println(originalWord)
+                randomWords.forEachIndexed { index, randomWord ->
+                    print(
+                        "${index + 1} - ${randomWord.translate}" +
+                                if (index != randomWords.lastIndex) ", " else "\n"
+                    )
+                }
+
+                val word = randomWords[randomWords.indexOfFirst { it.original == originalWord }]
+                when (readln()) {
+                    "0" -> break
+                    (randomWords.indexOf(word) + 1).toString() -> {
+                        println("Правильно!")
+                        dictionary[dictionary.indexOfFirst { it.original == originalWord }].correctAnswersCount++
+                        saveDictionary(dictionary)
+                    }
+
+                    else -> println("Неправильно - слово ${word.translate}")
+                }
             }
 
-            "2" -> {
-                val learned = dictionary.filter { it.correctAnswersCount > 3 }.size
-                val total = dictionary.size
-                val percent = learned * 100 / total
-                println("Выучено $learned из $total слов | $percent%")
-            }
+            "2" -> statistics(dictionary, learnTimes)
 
             "0" -> break
             else -> println("Неверно введенный номер пукта")
         }
     }
+}
+
+fun statistics(dictionary: MutableList<Word>, learnTimes: Int) {
+    val learned = dictionary.filter { it.correctAnswersCount >= learnTimes }.size
+    val total = dictionary.size
+    val percent = learned * 100 / total
+    println("Выучено $learned из $total слов | $percent%")
+}
+
+fun saveDictionary(dictionary: MutableList<Word>) {
+    val wordsFile = File("words.txt")
+    wordsFile.writeText("")
+    for (word in dictionary)
+        wordsFile.appendText("${word.original}|${word.translate}|${word.correctAnswersCount}\n")
 }
 
