5a9f439fa5da46803268bedef0ec9a149fef2708
Wonderfull23 <mywonderkotlin23@gmail.com>
2023-02-11 19:57:09 +0300

    stage_5 - added additional cycle for learning words, rename fun statistics to getStatistics

--- src/main/kotlin/additional/File.kt
+++ src/main/kotlin/additional/File.kt
@@ -37,48 +37,51 @@ fun menu(dictionary: MutableList<Word>) {
         println("Меню:\n1 – Учить слова\n2 – Статистика\n0 – Выход")
         when (readln()) {
             "1" -> {
-                val randomWords =
-                    dictionary.filter { it.correctAnswersCount < learnTimes }.shuffled().take(answerVariants)
-                        .toMutableList()
-                if (randomWords.isEmpty()) {
-                    println("Вы выучили все слова")
-                    break
-                } else if (randomWords.size < answerVariants)
-                    randomWords += dictionary.filter { it.correctAnswersCount >= learnTimes }.shuffled()
-                        .take(answerVariants - randomWords.size)
+                while (true) {
+                    val randomWords =
+                        dictionary.filter { it.correctAnswersCount < learnTimes }.shuffled().take(answerVariants)
+                            .toMutableList()
+                    if (randomWords.isEmpty()) {
+                        println("Вы выучили все слова")
+                        break
+                    } else if (randomWords.size < answerVariants)
+                        randomWords += dictionary.filter { it.correctAnswersCount >= learnTimes }.shuffled()
+                            .take(answerVariants - randomWords.size)
 
-                val originalWord = randomWords.filter { it.correctAnswersCount < learnTimes }.random().original
-                println(originalWord)
-                randomWords.forEachIndexed { index, randomWord ->
-                    print(
-                        "${index + 1} - ${randomWord.translate}" +
-                                if (index != randomWords.lastIndex) ", " else "\n"
-                    )
-                }
-
-                val word = randomWords[randomWords.indexOfFirst { it.original == originalWord }]
-
-                when (readln()) {
-                    "0" -> break
-                    (randomWords.indexOf(word) + 1).toString() -> {
-                        println("Правильно!")
-                        dictionary[dictionary.indexOfFirst { it.original == originalWord }].correctAnswersCount++
-                        saveDictionary(dictionary)
+                    val originalWord = randomWords.filter { it.correctAnswersCount < learnTimes }.random().original
+                    println(originalWord)
+                    print("0 - Выход в меню, ")
+                    randomWords.forEachIndexed { index, randomWord ->
+                        print(
+                            "${index + 1} - ${randomWord.translate}" +
+                                    if (index != randomWords.lastIndex) ", " else "\n"
+                        )
                     }
 
-                    else -> println("Неправильно - слово ${word.translate}")
+                    val word = randomWords[randomWords.indexOfFirst { it.original == originalWord }]
+
+                    when (readln()) {
+                        "0" -> break
+                        (randomWords.indexOf(word) + 1).toString() -> {
+                            println("Правильно!")
+                            dictionary[dictionary.indexOfFirst { it.original == originalWord }].correctAnswersCount++
+                            saveDictionary(dictionary)
+                        }
+
+                        else -> println("Неправильно - слово ${word.translate}")
+                    }
                 }
             }
 
-            "2" -> statistics(dictionary, learnTimes)
+            "2" -> getStatistics(dictionary, learnTimes)
 
             "0" -> break
-            else -> println("Неверно введенный номер пукта")
+            else -> println("Неверно введенный номер пункта")
         }
     }
 }
 
-fun statistics(dictionary: MutableList<Word>, learnTimes: Int) {
+fun getStatistics(dictionary: MutableList<Word>, learnTimes: Int) {
     val learned = dictionary.filter { it.correctAnswersCount >= learnTimes }.size
     val total = dictionary.size
     val percent = learned * 100 / total
